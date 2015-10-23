061495910bee78be52e28910df8c58c0aa1bb10b
ikawaha <ikawaha@users.noreply.github.com>
2015-10-23 11:39:38 +0900

    Add tests

--- tinysegmenter_test.go
+++ tinysegmenter_test.go
@@ -1,12 +1,40 @@
 package tinysegmenter
 
 import (
+	"io/ioutil"
+	"reflect"
 	"testing"
 )
+
+var sampletext string
+
+func init() {
+	b, err := ioutil.ReadFile("./timemachineu8j.txt")
+	if err != nil {
+		panic(err)
+	}
+	sampletext = string(b)
+}
+
+func TestSegment(t *testing.T) {
+	s := NewSegmenter()
+	ary := s.Segment("私の名前は中野です")
+	expect := []string{"私", "の", "名前", "は", "中野", "です"}
+	if !reflect.DeepEqual(ary, expect) {
+		t.Errorf("got %+v, expected %v", ary, expect)
+	}
+}
 
 func BenchmarkSegment(b *testing.B) {
 	s := NewSegmenter()
 	for i := 0; i < b.N; i++ {
 		s.Segment("私の名前は中野です")
+	}
+}
+
+func BenchmarkSegmentLargeText(b *testing.B) {
+	s := NewSegmenter()
+	for i := 0; i < b.N; i++ {
+		s.Segment(sampletext)
 	}
 }
